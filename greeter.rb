ada4648759e5f807faa040ff19ff00de73f50ae2
joeltomjacob <joel.jacob@ucdconnect.ie>
2017-09-18 12:15:22 +0100

    Initial commit

---
+++ greeter.rb
@@ -0,0 +1,27 @@
+
+class Greeter
+  
+  def initialize(name)
+    @name  = name
+  end
+  
+  def say_hi
+    puts "hello #{@name}"
+  end
+  
+  def say_bye
+    puts "bye #{@name}"
+  end
+  
+end
+
+my_greeter = Greeter.new('Matz')
+
+5.times do
+  my_greeter.say_hi
+  my_greeter.say_bye
+end
+
+if 1 > 0 
+  puts 'this is an example of an if statement'
+end
